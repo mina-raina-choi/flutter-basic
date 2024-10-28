655a921be20b0eb9faec70400785e0384b277425
Mina Choi <cmina2846@gmail.com>
2024-10-28 14:08:55 +0900

    flutter toonflix

--- lib/main.dart
+++ lib/main.dart
@@ -1,85 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
   runApp(const App());
 }
 
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({super.key});
-
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  bool showTitle = true;
-
-  void toggleTitle() {
-    setState(() {
-      showTitle = !showTitle;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        textTheme: const TextTheme(
-          titleLarge: TextStyle(
-            color: Colors.red,
-          ),
-        ),
-      ),
-      home: Scaffold(
-        backgroundColor: const Color(0xFFF4EDD8),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              showTitle ? const MyLargeTitle() : const Text('NONE'),
-              IconButton(
-                onPressed: toggleTitle,
-                icon: const Icon(Icons.toggle_on),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class MyLargeTitle extends StatefulWidget {
-  const MyLargeTitle({
-    super.key,
-  });
-
-  @override
-  State<MyLargeTitle> createState() => _MyLargeTitleState();
-}
-
-class _MyLargeTitleState extends State<MyLargeTitle> {
-  @override
-  void initState() {
-    super.initState();
-    print('1. initState');
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    print('3. dispose');
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    print('2. Build');
-    return Text(
-      'My Large Title',
-      style: TextStyle(
-        fontSize: 24,
-        color: Theme.of(context).textTheme.titleLarge!.color,
-      ),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
   }
 }
